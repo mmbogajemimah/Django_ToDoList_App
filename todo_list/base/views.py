8c90e45c4f7c72eec341100e98337f7b06d8df24
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-06-09 15:08:04 +0300

    Login functionality using generic views

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,16 +1,52 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # from django.http import HttpResponse
 from .models import Task
 
+class CustomLoginView(LoginView):
+   template_name = 'base/login.html'
+   fields = '__all__'
+   redirect_authenticated_user = True
+
+   def get_success_url(self):
+      return reverse_lazy('tasks')
 
 # Create your views here.
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
    model = Task
    context_object_name = 'tasks'
 
-class TaskDetail(DetailView):
+   def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      # Filter to to see tasks from a specific user
+      context['tasks'] = context['tasks'].filter(user=self.request.user)
+      context['count'] = context['tasks'].filter(complete=False).count()
+      return context
+
+class TaskDetail(LoginRequiredMixin, DetailView):
    model = Task
    context_object_name ='task'
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+   model = Task
+   fields = '__all__'
+   success_url = reverse_lazy('tasks')
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+   model = Task
+   fields = '__all__'
+   success_url = reverse_lazy('tasks')
+
+class DeleteView(LoginRequiredMixin, DeleteView):
+   model = Task
+   context_object_name = 'task'
+   success_url = reverse_lazy('tasks')
